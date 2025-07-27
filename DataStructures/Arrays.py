8b305261d3751cfe28c6560079f3a343f65617b5
KinyuaM <kinyuajames104@gmail.com>
2025-07-27 12:03:11 +0300

    Week 1 assignement submit

---
+++ DataStructures/Arrays.py
@@ -0,0 +1,18 @@
+# An array is a fixed size- hold together elements of a similar data type
+#index 0 1 2 3 4 
+arr = [10, 20, 30, 40, 50]
+
+print (arr[2])
+
+arr[2]= 70
+
+print(arr[2])
+
+arr.append(60)
+
+print (arr)
+
+if 30 in arr:
+    print ("Found 30")
+
+arr.insert 
---
+++ DataStructures/Assignment/calculator.py
@@ -0,0 +1,23 @@
+num1 =float(input("Enter your first number: "))
+num2 =float(input("Enter your second number: "))
+
+operation = input("Enter the operation (+, -, *, /)")
+
+if operation == "+":
+    result = num1+num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 !=0:
+        result = num1/num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed")
+else: 
+    print("Invalid operation. Please enter a valid operation")
+
